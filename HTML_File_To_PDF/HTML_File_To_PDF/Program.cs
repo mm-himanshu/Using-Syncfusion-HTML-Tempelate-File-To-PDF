be0fe441473417a008da65921e519d46650e7714
mm-himanshu <mm.himanshup@gmail.com>
2024-12-12 13:28:09 +0530

    HTML to PDF sample

---
+++ HTML_File_To_PDF/HTML_File_To_PDF/Program.cs
@@ -0,0 +1,24 @@
+﻿using Syncfusion.HtmlConverter;
+using Syncfusion.Pdf;
+using System.IO;
+
+namespace HTML_File_To_PDF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Initialize HTML to PDF converter with Blink rendering engine
+            HtmlToPdfConverter htmlConverter = new HtmlToPdfConverter();
+
+            //Convert HTML File to PDF
+            PdfDocument document = htmlConverter.Convert(Path.GetFullPath("../../../../../Data/html_file_converter.htm"));
+
+            FileStream fileStream = new FileStream("HTML_file_to_PDF.pdf", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+
+            //Save and close the PDF document 
+            document.Save(fileStream);
+            document.Close(true);
+        }
+    }
+}
